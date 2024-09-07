b9809305767ad89d22625c6b45eff04e5b768458
vivekop0 <sarkarjii534@gamil.com>
2024-09-07 15:36:47 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
 // App.tsx or App.jsx
 
-import { BrowserRouter as Router, Routes, Route,Link} from 'react-router-dom';
-import { ContactUs } from './pages/ContactUs';
-
-
-
-
-
-
+import {  Routes, Route, Link } from "react-router-dom";
+import { ContactUs } from "./pages/ContactUs";
 
 function App() {
   return (
-<div>
-<h1>
-      <Link to={'/contact'}>Go to contact page</Link>
-     </h1>
-    <Router>
-     
-      
+    <div>
+      <h1>
+        <Link to={"/contact"}>Go to contact page</Link>
+      </h1>
 
       <Routes>
-      
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
-    </Router>
     </div>
   );
 }
